5c6bc3ebc61c393cedd428f8bb21c46a0e460391
Jay Potter <j@ypotter.ca>
2024-03-30 11:59:00 -0400

    Create OutputBufferAwareInterface.php

---
+++ src/Potter/Buffer/Output/Aware/OutputBufferAwareInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Buffer\Output\Aware;
+
+use \Potter\Buffer\Output\OutputBufferInterface;
+
+interface OutputBufferAwareInterface 
+{
+    public function getOutputBuffer(): OutputBufferInterface;
+    public function hasOutputBuffer(): bool;
+}
